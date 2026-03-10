48feb2c3bc878956aacba8766e44b0f9e7c067e1
Shaxriyor Qurbonov <shaxriyorqurbonov677@gmail.com>
2026-03-10 09:25:41 +0500

    CRUD

---
+++ 7-dars/script.js
@@ -0,0 +1,53 @@
+let array = ["salom", 234, true, {
+    name: "Abror"
+}]
+
+let Talaba = {
+    name: "Farhod",
+    suename : "Fozilov",
+    age: 23
+}
+
+let mevalar = ["nok", "olma", "anor", "behi",]
+
+for (const i of mevalar) {
+    console.log(i);
+}
+
+let data = [
+    {
+        name : "Axror",
+        surname:"Azizov",
+        age: 24,
+        isMarried: false
+    },
+
+    {
+        name : "Axror",
+        surname:"Azizov",
+        age: 24,
+        isMarried: false
+    },
+
+    {
+        name : "Axror",
+        surname:"Azizov",
+        age: 24,
+        isMarried: false
+    },
+]
+
+// String methodlar
+
+let text = "Javascript";
+console.log(text.length);
+console.log([0]);
+
+let str = "Salom Dunyo";
+console.log(str.indexOf("Dunyo"));
+console.log(str.lastIndexOf("n"));
+console.log(str.includes("Sal"));
+
+
+
+
