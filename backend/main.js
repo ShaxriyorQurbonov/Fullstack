3af73a4622bec44681cb57a0a441cf0ad195f03f
Shaxriyor Qurbonov <shaxriyorqurbonov677@gmail.com>
2026-04-14 09:18:01 +0500

    Backend lessons in JS

--- backend/main.js
+++ backend/main.js
@@ -1,4 +1,38 @@
 const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, 'user.json');
+
+function addUser(user) {
+    const newUser = {
+        name: user.name,
+        surname: user.surname,
+        age: user.age
+    };
+
+    fs.readFile(usersFilePath, 'utf8', (err, data) => {
+        let users = [];
+
+        if (!err && data) {
+            users = JSON.parse(data);
+        }
+
+        if (!Array.isArray(users)) {
+            users = [];
+        }
+
+        users.push(newUser);
+
+        fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                console.error('Error writing user:', err);
+                return;
+            }
+
+            console.log('User added successfully!');
+        });
+    });
+}
 
 fs.readFile('data.txt', 'utf8', (err, data) => {
     if (err) { 
@@ -9,3 +43,22 @@ fs.readFile('data.txt', 'utf8', (err, data) => {
     console.log( data);
 });
 
+// fs.writeFile('data.txt', 'Hello, World!', (err) => {
+//     if (err) {
+//         console.error('Error writing file:', err);
+//         return;
+//     }
+
+//     console.log('File written successfully!');
+// });
+
+// fs.appendFile('data.txt', '\nAppended text.', (err) => {
+//     if (err) {
+//         console.error('Error appending to file:', err);
+//         return;
+//     }
+
+//     console.log('Text appended successfully!');
+// }); 
+
+addUser({ name: 'John', surname: 'Doe', age: 30 });
